a860f408e0dff975ec00570c1e9172caa1a41311
ding.luchao <ding.luchao@theadventus.com>
2015-08-22 21:57:30 +0800

    Package

---
+++ myjava/eclipse/src/com/xiaoming/xiaoming.java
@@ -0,0 +1,16 @@
+package com.xiaoming;
+
+public class xiaoming {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
+class Dog{
+	
+}
---
+++ myjava/eclipse/src/com/xiaoqiang/xiaoqiang.java
@@ -0,0 +1,16 @@
+package com.xiaoqiang;
+import java.util.*; 
+public class xiaoqiang {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		HashMap hm = new HashMap();
+	}
+
+}
+class Dog{
+	
+}
