32227006a5f75f2c470d985791136a87a7e6380f
ding.luchao <ding.luchao@theadventus.com>
2015-09-11 16:11:34 +0800

    bak

---
+++ myjava/eclipse/src/tank7_bak/Member.java
@@ -0,0 +1,459 @@
+package tank7_bak;
+import java.awt.*;
+import java.util.*;
+/**
+ * 抽象类坦克
+ * @author Administrator
+ *
+ */
+abstract class Tanks{
+	//定义血
+	int life = 9;
+	//定义位置
+	int x = 0;
+	int y = 0;
+	//定义速度
+	int speed = 20;
+	//定义方向 1上 2右 0下 3左
+	int dir = 0;
+	//定义大小的 构建坦克后初始化
+	int sizeH = 0;
+	int sizeCz = 0;
+	//身份 0 自己的坦克人1 坏人
+	int identity = 0;
+	//颜色
+	Color color = null;
+	//是否活着
+	Boolean isLive = true;
+	//子弹
+	//限制一次可以发多少子弹
+	private int bulletNum = 5;
+	//Bullet bullet = null;
+	Vector<Bullet> bulletList = new Vector<Bullet>();
+	//clear BulletList
+	public void clearBulletList(Bullet bullet){
+		this.bulletList.remove(bullet);
+	}
+	public int getBulletNum() {
+		return bulletNum;
+	}
+	public int getLife() {
+		return life;
+	}
+	public void setLife(int life) {
+		this.life = life;
+	}
+	public Boolean getIsLive() {
+		return isLive;
+	}
+	public void setIsLive(Boolean isLive) {
+		this.isLive = isLive;
+	}
+	public void setBulletNum(int bulletNum) {
+		this.bulletNum = bulletNum;
+	} 
+	public Color getColor() {
+		return color;
+	}
+	public void setColor(Color color) {
+		this.color = color;
+	}
+	public int getIdentity() {
+		return identity;
+	}
+	public void setIdentity(int identity) {
+		this.identity = identity;
+	}
+	public int getSizeCz() {
+		return sizeCz;
+	}
+	public void setSizeCz(int sizeCz) {
+		this.sizeCz = sizeCz;
+	}
+	 
+	public int getSizeH() {
+		return sizeH;
+	}
+	public void setSizeH(int sizeH) {
+		this.sizeH = sizeH;
+	}
+	public int getDir() {
+		return dir;
+	}
+	public void setDir(int dir) {
+		this.dir = dir;
+	}
+	public int getSpeed() {
+		return speed;
+	}
+	public void setSpeed(int speed) {
+		this.speed = speed;
+	}
+	public int getX() {
+		return x;
+	}
+	public void setX(int x) {
+		this.x = x;
+	}
+	public int getY() {
+		return y;
+	}
+	public void setY(int y) {
+		this.y = y;
+	}
+	
+	public Tanks(int x, int y){
+	    this.x = x;
+	    this.y = y;
+	}
+	//向上移动
+	public void moveTop(){
+		if(y > sizeCz){
+			y -= speed;
+			if(y < sizeCz){
+				y = sizeCz;
+			}
+		}
+		dir = 1;
+	}
+	public void moveRight(int border){
+		int rightX = border - this.getSizeH() - 6;
+		if(x < rightX){//这里解释下为什么在减去一个cz是因为你创建的400宽实际达不到弹出的JFrame是有边框的6像素左右。
+			x += speed;
+			if(x > rightX){
+				x = rightX;
+			}
+		}
+		dir = 2;
+	}
+	public void moveDown(int border){
+		int bottomY = border - this.getSizeH();
+		if(y < bottomY){
+			y += speed;
+			if(y > bottomY){
+				y = bottomY;
+			}
+		}
+		dir = 0;
+	}
+	public void moveLeft(){
+		if(x > sizeCz){
+			x -= speed;
+			if(x < sizeCz){
+				x = sizeCz;
+			}
+		}
+		dir = 3;
+	}
+	//开火
+	public void fire(int borderX, int borderY){
+		int x = 0;
+		int y = 0;
+		//定义方向 1上 2右 0下 3左
+		switch(this.dir){
+		case 0:
+			x = this.x + 7;
+			y = this.y + 36;
+			;break;
+		case 1:
+			x = this.x + 7;
+			y = this.y - 11;
+			;break;
+		case 2:
+			x = this.x + 36;
+			y = this.y + 7;
+			;break;
+		case 3:
+			x = this.x -11;
+			y = this.y + 8;
+			;break;
+		}
+		//启动子弹线程
+		Bullet s = new Bullet(x, y, this.dir, borderX, borderY);
+		this.bulletList.add(s);
+		Thread thread = new Thread(s);
+		thread.start();
+	}
+}
+/**
+ * 好坦克
+ * @author Administrator
+ *
+ */
+class GoodTanks extends Tanks{
+	public GoodTanks(int x, int y){
+		super(x, y);
+		this.setIdentity(0);
+		this.setDir(1);
+		this.setColor(Color.yellow);	
+	}	
+}
+/**
+ * 坏坦克
+ * @author Administrator
+ *
+ */
+class BadTanks extends Tanks implements Runnable{
+	public BadTanks(int x, int y){
+		super(x, y);
+		this.setIdentity(1);
+		this.setColor(Color.red);
+		this.setSpeed(1);
+		this.setLife(1);
+	}
+	
+	@Override
+	//坦克的线程
+	public void run() {
+		// TODO Auto-generated method stub
+		while(true){
+			
+			for(int i = 0; i < 10; i++){
+				//定义方向 1上 2右 0下 3左
+				switch(this.dir){
+				case 0:
+					this.moveDown(300);
+					break;
+				case 1:
+					this.moveTop();
+					break;
+				case 2:
+					this.moveRight(400);
+					break;
+				case 3:
+					this.moveLeft();
+					break;
+				}
+				try {
+					Thread.sleep(50);
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+			
+			//动态修改随机的方向
+			this.dir = (int)(Math.random() * 4);
+			if(!this.isLive){
+				break;
+			}
+			
+			//子弹达到一定量就不发了
+			if(this.bulletList.size() < this.getBulletNum()){
+				this.fire(400, 300);
+			}
+			
+		}
+	}
+}
+/**
+* 子弹
+* 具有线程类
+*/
+class Bullet implements Runnable{
+	Color color = null;
+	int speed = 2;
+	int x = 0;
+	int y = 0;
+	int dir = 0;
+	//子弹的尺寸
+	int width = 5;
+	int height = 5;
+	//边界
+	int borderX = 0;
+	int borderY = 0;
+	//子弹是否还活着
+	Boolean isLive = true;
+	
+	
+	public int getWidth() {
+		return width;
+	}
+	public void setWidth(int width) {
+		this.width = width;
+	}
+	public int getHeight() {
+		return height;
+	}
+	public void setHeight(int height) {
+		this.height = height;
+	}
+	public int getBorderX() {
+		return borderX;
+	}
+	public void setBorderX(int borderX) {
+		this.borderX = borderX;
+	}
+	public int getBorderY() {
+		return borderY;
+	}
+	public void setBorderY(int borderY) {
+		this.borderY = borderY;
+	}
+	public Boolean getIsLive() {
+		return isLive;
+	}
+	public void setIsLive(Boolean isLive) {
+		this.isLive = isLive;
+	}	
+	public int getDir() {
+		return dir;
+	}
+	public void setDir(int dir) {
+		this.dir = dir;
+	}
+	public Color getColor() {
+		return color;
+	}
+	public void setColor(Color color) {
+		this.color = color;
+	}
+	public int getSpeed() {
+		return speed;
+	}
+	public void setSpeed(int speed) {
+		this.speed = speed;
+	}
+	public int getX() {
+		return x;
+	}
+	public void setX(int x) {
+		this.x = x;
+	}
+	public int getY() {
+		return y;
+	}
+	public void setY(int y) {
+		this.y = y;
+	}
+
+	public Bullet(int x, int y, int dir, int borderX, int borderY){
+		this.x = x;
+		this.y = y;
+		this.dir = dir;
+		this.color = Color.black;
+		this.borderX = borderX;
+		this.borderY = borderY;
+	}
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		//max x max y min x min y
+		int maxX = this.borderX - this.width - 6; //减去6个像素的边框
+		int maxY = this.borderY - this.height;
+		int minX = 0;
+		int minY = 0;
+		while(true){
+			//定义方向 1上 2右 0下 3左
+			switch(this.dir){
+				case 0:
+					if(this.y + this.speed < maxY){
+						this.y += this.speed;
+					}else{
+						this.y = maxY;
+					}
+					;break;
+				case 1:
+					if(this.y > minY){
+						this.y -= this.speed;
+					}else{
+						this.y = minY;
+					}
+					;break;
+				case 2:
+					if(this.x + this.speed < maxX){
+						this.x += this.speed;
+					}else{
+						this.x = maxX;
+					}
+					;break;
+				case 3:
+					if(this.x > minX){
+						this.x -= this.speed;
+					}else{
+						this.x = minX;
+					}
+					;break;
+			}
+			 
+			//子弹何时死亡
+			if(x == minX || x == maxX || y == minY || y == maxY){
+				this.isLive = false;
+				
+				break;
+			}
+			
+			try {
+				Thread.sleep(10);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+
+		}
+	}
+}
+
+class Bomb implements Runnable{	
+ private int x = 0;
+ private int y = 0;
+ //炸弹的生命
+ private int life = 9;
+ private Boolean isLive = true;
+ public Bomb(int x, int y){
+	 this.x = x;
+	 this.y = y;
+ }
+
+ public int getX() {
+	return x;
+	}
+	public void setX(int x) {
+		this.x = x;
+	}
+	public int getY() {
+		return y;
+	}
+	public void setY(int y) {
+		this.y = y;
+	}
+	public int getLife() {
+		return life;
+	}
+	public void setLife(int life) {
+		this.life = life;
+	}
+	public Boolean getIsLive() {
+		return isLive;
+	}
+	public void setIsLive(Boolean isLive) {
+		this.isLive = isLive;
+	}
+ 
+ //减少生命
+ public void LifeDown(){
+	 if(this.life > 0){
+		 this.life --;
+	 }else{
+		 this.isLive = false;
+	 }
+ }
+
+@Override
+public void run() {
+	// TODO Auto-generated method stub
+	while(true){
+		if(this.isLive){
+			this.LifeDown();
+		}else{
+			break;
+		}
+		try {
+			Thread.sleep(20);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+}
+}
---
+++ myjava/eclipse/src/tank7_bak/tank.java
@@ -0,0 +1,486 @@
+package tank7_bak;
+import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.util.*;
+import java.io.*;
+
+import javax.imageio.ImageIO;
+import javax.swing.*;
+public class tank extends JFrame{
+	public TankPanel tp = null;
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		tank init = new tank();
+	}
+	
+	public tank(){
+		this.tp = new TankPanel();
+		//启动刷新线程
+		Thread thread = new Thread(this.tp);
+		thread.start();
+		this.add(tp);
+		this.setTitle("丁陆超PK姜丽丽");
+		this.setSize(400,400);
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setResizable(false);
+		this.setVisible(true);
+		this.addKeyListener(this.tp);
+	}
+
+}
+
+/**
+ * 坦克的面板
+ * 因为要实现坦克动起来所以必须要这个TankPanel监听键盘的事件
+ * @author Administrator
+ *
+ */
+class TankPanel extends JPanel implements KeyListener,Runnable{
+	
+	/**
+	 * 整个有戏都是生存在这个panel里面
+	 */
+	//定义我的坦克
+	private GoodTanks myTank = null;
+	//定义坏人坦克,线程机制使用Vector
+	private Vector<BadTanks> badTanksList = new Vector<BadTanks>();
+	//private BadTanks badTank = null;
+	
+	//定义爆炸的图片
+	private Image image1 = null;
+	private Image image2 = null;
+	private Image image3 = null;
+	//定义炸弹的向量
+	Vector<Bomb> bombList = new Vector<Bomb>();
+	
+	//定义活动范围
+	private int activityW = 400;
+	private int activityH = 300;
+	//定义初始化多少个敌人坦克
+	private int numBadTank = 5;
+	
+	
+	public TankPanel(){
+		this.myTank = new GoodTanks(100,264);
+		//this.badTank = new BadTanks(200,200); 
+		//初始话敌人的坦克
+		for(int i = 0; i < this.numBadTank; i++){
+			BadTanks badTank = new BadTanks((i+1)*60,0);
+			this.badTanksList.add(badTank);
+			Thread thread = new Thread(badTank);
+			thread.start();
+
+			//给敌人加上武器子弹
+			badTank.fire(this.activityW, this.activityH);
+		}
+		
+		//初始化爆炸图片--三张图片组合为一个炮炸效果
+		/*this.image1 = Toolkit.getDefaultToolkit().getImage(Panel.class.getResource("/tp/bz.png"));
+		this.image2 = Toolkit.getDefaultToolkit().getImage(Panel.class.getResource("/tp/bz2.png"));
+		this.image3 = Toolkit.getDefaultToolkit().getImage(Panel.class.getResource("/tp/bz3.png"));*/
+		try{
+			//相对于test这层目录
+			//先把图片调到内存里面里面来否则第一次会有问题，当然我用线程已然解决了这个问题。但是这也是个思路
+			this.image1 = ImageIO.read(new File("tp\\bz.png"));
+			this.image2 = ImageIO.read(new File("tp\\bz2.png"));
+			this.image3 = ImageIO.read(new File("tp\\bz3.png"));
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+	}
+	
+	public void paint(Graphics g){
+		//第一次调用的时候创建两个坦克，然后每次调用repaint的时候是在原对象基础上跑
+		super.paint(g);
+		
+		//其实这个相当于一个动画每次都需要在画布上重新绘制这样看起来就像是动态的，每次重绘原来的就不存在了要重新绘制一次，这样看起来就像是在动。
+		//repaint 相当于在画布上重新绘制图像，原来的都擦掉了。
+		
+		//设置JPanel的活动区域
+		g.setColor(Color.gray);
+		g.fillRect(0, 0, this.activityW, this.activityH);
+		
+		//开始画自己坦克图片
+		if(this.myTank.isLive){
+			this.createTank(this.myTank.getX(), this.myTank.getY(), g, this.myTank.getDir(), this.myTank.getIdentity(), this.myTank, this.myTank.getColor());
+		}else{
+			g.setColor(Color.BLACK);
+			g.fillRect(0, 0, this.activityW, this.activityH);
+			g.setColor(Color.RED);
+			g.drawString("Game over", 150, 140);
+			return ;
+		}
+		//敌人坦克
+		for(int i = 0; i < this.badTanksList.size(); i++){
+			BadTanks bt = this.badTanksList.get(i);
+			if(bt.isLive){
+				this.createTank(bt.getX(), bt.getY(), g, bt.getDir(), bt.getIdentity(), bt, bt.getColor());
+				//绘制敌人坦克
+				for(int j = 0; j < bt.bulletList.size(); j++){
+					Bullet bullet = bt.bulletList.get(j);
+					if(bullet.isLive){
+						this.createBulleat(bullet.getX(),bullet.getY(), g, bullet.getColor(), bullet.getWidth(), bullet.getHeight());
+					}else{
+						bt.bulletList.remove(bullet);
+					}
+				}
+			}else{
+				this.badTanksList.remove(bt);
+			}
+		}
+		
+		//自己的子弹
+		for(int i = 0; i < this.myTank.bulletList.size(); i++){
+			Bullet bullet = this.myTank.bulletList.get(i);
+			if(bullet.isLive == true){
+				this.createBulleat(bullet.getX(),bullet.getY(), g, bullet.getColor(), bullet.getWidth(), bullet.getHeight());
+			}else{
+				if(bullet != null){
+					this.myTank.clearBulletList(bullet);	
+				}
+				
+			}
+		}
+		
+		 
+		
+		//开始绘制爆炸的图像
+				for(int i = 0; i < this.bombList.size(); i++){
+					Bomb b = this.bombList.get(i);
+					if(b.getIsLive()){
+						this.bombPlay(b, g);
+					}else{
+						//从队列删除
+						this.bombList.remove(b);
+					}
+				}
+	}
+	private void bombPlay(Bomb b, Graphics g){
+		int bs = (int)Math.ceil((double)b.getLife() / 3);
+		switch(bs){
+		case 3:
+			g.drawImage(this.image1, b.getX(), b.getY(), 30, 30, this);
+			break;
+		case 2:
+			g.drawImage(this.image2, b.getX(), b.getY(), 30, 30, this);
+			break;
+		case 1:
+			g.drawImage(this.image3, b.getX(), b.getY(), 30, 30, this);
+			break;
+		}		 
+	}
+	private void setTankAndBulleatStatus(Bullet b, Tanks tk){
+		//定义方向 1上 2右 0下 3左
+		Boolean isHit = false;
+		int btMaxX = b.getX() + b.getWidth();
+		int btMaxY = b.getY() + b.getHeight();
+		int btMinX = b.getX();
+		int btMinY = b.getY();
+		
+		int tkPTMaxX = 0;
+		int tkPTMaxY = 0;
+		int tkPTMinX = 0;
+		int tkPTMinY = 0;
+		int tkMaxX = 0;
+		int tkMaxY = 0;
+		int tkMinX = 0;
+		int tkMinY = 0;
+		//定义方向 1上 2右 0下 3左
+		switch(tk.dir){
+		case 0:
+			//炮管
+			tkPTMaxX = tk.getX() + 8 + 4;
+			tkPTMinX = tk.getX() + 8;
+			tkPTMaxY = tk.getY() + 5;
+			tkPTMinY = tk.getY();
+			//坦克
+			tkMaxX = tk.getX() + 20;
+			tkMinX = tk.getX();
+			tkMaxY = tk.getY() + 30;
+			tkMinY = tk.getY();
+			if((btMinX < tkPTMaxX && btMinX > tkPTMinX && btMinY < tkPTMaxY && btMinY > tkPTMinY) 
+			|| 
+			(btMaxX < tkPTMaxX && btMaxX > tkPTMinX && btMaxY < tkPTMaxY && btMaxY > tkPTMinY)
+			||
+			(btMinX < tkMaxX && btMinX > tkMinX && btMinY < tkMaxY && btMinY > tkMinY)
+			||
+			(btMaxX < tkMaxX && btMaxX > tkMinX && btMaxY < tkMaxY && btMaxY > tkMinY)
+			){
+				isHit = true;
+			}
+			
+			
+			;break;
+		case 1:
+			
+			//炮管
+			tkPTMaxX = tk.getX() + 8 + 4;
+			tkPTMinX = tk.getX() + 8;
+			tkPTMaxY = tk.getY();
+			tkPTMinY = tk.getY() - 5;
+			//坦克
+			tkMaxX = tk.getX() + 20;
+			tkMinX = tk.getX();
+			tkMaxY = tk.getY() + 30;
+			tkMinY = tk.getY();
+			if((btMinX < tkPTMaxX && btMinX > tkPTMinX && btMinY < tkPTMaxY && btMinY > tkPTMinY) 
+			|| 
+			(btMaxX < tkPTMaxX && btMaxX > tkPTMinX && btMaxY < tkPTMaxY && btMaxY > tkPTMinY)
+			||
+			(btMinX < tkMaxX && btMinX > tkMinX && btMinY < tkMaxY && btMinY > tkMinY)
+			||
+			(btMaxX < tkMaxX && btMaxX > tkMinX && btMaxY < tkMaxY && btMaxY > tkMinY)
+			){
+				isHit = true;
+			}
+			
+			;break;
+		case 2:
+			
+			//炮管
+			tkPTMaxX = tk.getX() + 12 + 5;
+			tkPTMinX = tk.getX() + 12;
+			tkPTMaxY = tk.getY() + 8 + 4;
+			tkPTMinY = tk.getY() + 8;
+			//坦克
+			tkMaxX = tk.getX() + 20;
+			tkMinX = tk.getX();
+			tkMaxY = tk.getY() + 30;
+			tkMinY = tk.getY();
+			if((btMinX < tkPTMaxX && btMinX > tkPTMinX && btMinY < tkPTMaxY && btMinY > tkPTMinY) 
+			|| 
+			(btMaxX < tkPTMaxX && btMaxX > tkPTMinX && btMaxY < tkPTMaxY && btMaxY > tkPTMinY)
+			||
+			(btMinX < tkMaxX && btMinX > tkMinX && btMinY < tkMaxY && btMinY > tkMinY)
+			||
+			(btMaxX < tkMaxX && btMaxX > tkMinX && btMaxY < tkMaxY && btMaxY > tkMinY)
+			){
+				isHit = true;
+			}
+			
+			;break;
+		case 3:
+			
+			//炮管
+			tkPTMaxX = tk.getX() - 5 + 5;
+			tkPTMinX = tk.getX() - 5;
+			tkPTMaxY = tk.getY() + 8 + 4;
+			tkPTMinY = tk.getY() + 8;
+			//坦克
+			tkMaxX = tk.getX() + 20;
+			tkMinX = tk.getX();
+			tkMaxY = tk.getY() + 30;
+			tkMinY = tk.getY();
+			if((btMinX < tkPTMaxX && btMinX > tkPTMinX && btMinY < tkPTMaxY && btMinY > tkPTMinY) 
+			|| 
+			(btMaxX < tkPTMaxX && btMaxX > tkPTMinX && btMaxY < tkPTMaxY && btMaxY > tkPTMinY)
+			||
+			(btMinX < tkMaxX && btMinX > tkMinX && btMinY < tkMaxY && btMinY > tkMinY)
+			||
+			(btMaxX < tkMaxX && btMaxX > tkMinX && btMaxY < tkMaxY && btMaxY > tkMinY)
+			){
+				isHit = true;
+			}
+			
+			;break;
+		}
+		
+		if(isHit){
+			//算血量如果血量够就减去血不死
+			int life = tk.getLife();
+			if(life < 1){
+				tk.isLive = false;
+			}else{
+				tk.setLife(--life);
+			}
+			
+			b.isLive = false;
+			//如果击中了坦克就创建一个炸弹。
+			Bomb bob = new Bomb(tk.getX(),tk.getY());
+			Thread thread = new Thread(bob);
+			thread.start();
+			this.bombList.add(bob);
+		}
+	}
+	
+	private void createBulleat(int x, int y, Graphics g, Color color, int width, int height){
+		g.setColor(color);
+		g.fill3DRect(x, y, width, height, false);
+	}
+	private void createTank(int x, int y, Graphics g, int dir, int type, Tanks tk, Color color){
+		//色彩
+		/*Color color = color;
+		if(type == 0){
+			color = Color.cyan;
+		}else{
+			color = Color.yellow;
+		}*/
+		
+			
+		switch(dir){
+		case 0:
+			//左边轮子
+			g.setColor(color);
+			g.fill3DRect(x, y, 5, 30, false);
+							
+			//右边轮子
+			g.fill3DRect(x+15, y, 5, 30, false);
+									
+			//座
+			g.fill3DRect(x+5, y+5, 10, 20,false);
+			g.fillOval(x+5, y+10, 9, 8);
+									
+			//爆筒子
+			g.fillRect(x+8, y+12, 4, 23);
+			;break;//下
+		case 1:
+			//左边轮子
+			g.setColor(color);
+			g.fill3DRect(x, y, 5, 30, false);
+							
+			//右边轮子
+			g.fill3DRect(x+15, y, 5, 30, false);
+									
+			//座
+			g.fill3DRect(x+5, y+5, 10, 20,false);
+			g.fillOval(x+5, y+10, 9, 8);
+									
+			//爆筒子
+			g.fillRect(x+8, y-5, 4, 23);
+			;break;//上
+		case 2:
+			//左边轮子
+			g.setColor(color);
+			g.fill3DRect(x, y, 30, 5, false);
+							
+			//右边轮子
+			g.fill3DRect(x, y+15, 30, 5, false);
+									
+			//座
+			g.fill3DRect(x+5, y+5, 20, 10,false);
+			g.fillOval(x+10, y+5, 8, 9);
+									
+			//爆筒子
+			g.fillRect(x+12, y+8, 23, 4);
+			;break;//右
+		case 3:
+			//左边轮子
+			g.setColor(color);
+			g.fill3DRect(x, y, 30, 5, false);
+							
+			//右边轮子
+			g.fill3DRect(x, y+15, 30, 5, false);
+									
+			//座
+			g.fill3DRect(x+5, y+5, 20, 10,false);
+			g.fillOval(x+10, y+5, 8, 9);
+									
+			//爆筒子
+			g.fillRect(x-5, y+8, 23, 4);
+			;break;//左
+		}
+		
+		//第一次初始化坦克们的尺寸 结果根据下面的计算得出
+		if(tk.getSizeH() == 0){
+			tk.setSizeH(36);
+			tk.setSizeCz(6);
+		}
+		
+	}
+
+	@Override
+	public void keyTyped(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		// TODO Auto-generated method stub
+		//判断是不是方向键
+		int dir = -1;
+		//set tank position
+		switch(e.getKeyCode()){
+		case KeyEvent.VK_UP:
+			this.myTank.moveTop();
+			dir = 1;
+			;break;
+		case KeyEvent.VK_RIGHT:
+			dir = 2;
+			this.myTank.moveRight(this.activityW);
+			;break;
+		case KeyEvent.VK_DOWN:
+			dir = 0;
+			this.myTank.moveDown(this.activityH);
+			;break;
+		case KeyEvent.VK_LEFT:
+			dir = 3;
+			this.myTank.moveLeft();
+			;break;
+		}
+		
+		if(e.getKeyCode() == KeyEvent.VK_SPACE && this.myTank.bulletList.size() < this.myTank.getBulletNum()){
+			//开火
+			this.myTank.fire(this.activityW, this.activityH);
+			dir = 4;
+		}
+		
+		//点击的其他按键
+		if(dir!=-1){
+			this.repaint();
+		}
+	}
+	//好人打坏人
+	private void setIsLive(Vector<Bullet> b, Vector<BadTanks> tk){	
+		int len = b.size();
+		int badTkLen = tk.size();
+		for(int i = 0; i < len; i++){
+			for(int j = 0; j < badTkLen; j++){
+				this.setTankAndBulleatStatus(b.get(i), tk.get(j));
+			}
+		}		
+	}
+	
+	//坏人打好人
+	private void badKillGood(Vector<BadTanks> badList, GoodTanks tk){	
+	    for(int i = 0; i < badList.size(); i++){
+	    	BadTanks badTk = badList.get(i);
+	    	for(int j = 0; j < badTk.bulletList.size(); j++){
+	    		 this.setTankAndBulleatStatus(badTk.bulletList.get(j), tk);
+	    	}
+	    }
+	}
+	
+	
+	@Override
+	public void keyReleased(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	//刷新线程
+	public void run() {
+		// TODO Auto-generated method stub
+		while(true){
+			try {
+				Thread.sleep(5);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+			//好人打坏人的子弹和坦克状态
+			this.setIsLive(this.myTank.bulletList, this.badTanksList);
+			//坏人干掉好人
+			this.badKillGood(this.badTanksList, this.myTank);
+			
+			
+			this.repaint();
+		}
+	}
+}
